9393be86a1bcec5a23009e710c8a5092d156e8d2
Marc Gàllego <mga2525@gmail.com>
2019-05-03 23:49:30 +0200

    Update data.py

--- data.py
+++ data.py
@@ -4,10 +4,24 @@ import haversine as hs
 import geopy as gp
 import staticmap as smap
 
-
+def addressesTOcoordinates(addresses):
+    '''
+    Returns the two coordinates of two addresses of Barcelona
+    in a single string separated by a comma. In case of failure, returns None.
+    '''
+    try:
+        geolocator = Nominatim(user_agent="bicing_bot")
+        address1, address2 = addresses.split(',')
+        location1 = geolocator.geocode(address1 + ', Barcelona')
+        location2 = geolocator.geocode(address2 + ', Barcelona')
+        return (location1.latitude, location1.longitude), (location2.latitude, location2.longitude)
+    except:
+        return None
 
 
 def main():
+    url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
+    bicing = pd.DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
     G = nx.Graph()
 
 
