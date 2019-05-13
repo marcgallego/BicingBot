bc605bf76950278bceb8f08c6aef8d5fade1ef71
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2019-05-13 16:50:08 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,18 +1,34 @@
 import telegram
 from telegram.ext import Updater, CommandHandler
+import networkx as nx
+import os
 
-##PROVISIONAL
-from mapa import dibuixaMapa
+from dataBasic import dibuixaMapa, creaGraf
+
+'''
+Coses que falten a fer:
+    --funcio components i route
+    --comprovar que funciona per més d'un usuari a la vegada
+    --acabar d'arreglar els textos
+    --fer alguna cosa extra per anar de sobrats(amb la capacitat de cada estacio o la altitud)
+    --posar algun easteregg aixi fino per las risas
+
+Comentaris:
+    -- segons la documentacio el graf per defecte ha de ser de mida 1000.
+            Ara es crea a /start pero no entenc molt be com va la cosa
+    --Hi ha un diccionari per relacionar cada id d'usuari amb el seu graf, nose si funciona
+'''
 
 #textos
-startText = "Hola! Soc un bot de bicicletes"
+startText = "Hola! Sóc un bot del Bicing de Barcelona. \nPer més informació escriu /help."
 helpText = "Coses que puc fer i presentacio i ajuda i tal i cual"
-authorsText = "Marc Gallego i Marc Vernet i blablabla"
+authorsText = "Els meus autors són: \nMarc Gallego: marc.gallego.asin@est.fib.upc.edu \nMarc Vernet:marc.vernet@est.fib.upc.edu"
 
+usuaris = dict()
 
-#funcions
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=startText)
+    usuaris[update.message.chat_id] = creaGraf(1)
 
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=helpText)
@@ -21,27 +37,27 @@ def authors(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=authorsText)
 
 def graph(bot, update, args):
-    bot.send_message(chat_id=update.message.chat_id, text="algo")
+    usuaris[update.message.chat_id] = creaGraf(int(args[0])/1000)
+    bot.send_message(chat_id=update.message.chat_id, text="graf fet")
 
 def nodes(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="algo")
+    n = usuaris[update.message.chat_id].number_of_nodes()
+    bot.send_message(chat_id=update.message.chat_id, text=n)
 
 def edges(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="algo")
+    n = usuaris[update.message.chat_id].number_of_edges()
+    bot.send_message(chat_id=update.message.chat_id, text=n)
 
 def components(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="algo")
 
 def plotgraph(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="algo")
+    dibuixaMapa(usuaris[update.message.chat_id])
+    bot.send_photo(chat_id=update.message.chat_id, photo=open('map.png', 'rb'))
+    os.remove("map.png")
 
 def route(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text="algo")
-
-###PROVISIONAL
-def estacions(bot, update):
-    #dibuixaMapa()  Molt curios que no cal cridar la funcio perque al importar el fitxer ja s'executa tot
-    bot.send_photo(chat_id=update.message.chat_id, photo=open('map.png', 'rb'))
 
 
 # declara una constant amb el access token que llegeix de token.txt
@@ -61,9 +77,6 @@ dispatcher.add_handler(CommandHandler('edges', edges))
 dispatcher.add_handler(CommandHandler('components', components))
 dispatcher.add_handler(CommandHandler('plotgraph', plotgraph))
 dispatcher.add_handler(CommandHandler('route', route, pass_args=True))
-
-##PROVISIONAL
-dispatcher.add_handler(CommandHandler('mapa', estacions))
 
 # engega el bot
 updater.start_polling()
