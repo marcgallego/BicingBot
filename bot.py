890b8b896cfc67ab3b0d95b91b7d31dec23f2c03
marcvernet31 <44305244+marcvernet31@users.noreply.github.com>
2019-05-19 00:46:54 +0200

    bot i data ajuntats i funcionals (falten cosetes)

--- bot.py
+++ bot.py
@@ -5,29 +5,21 @@ import random
 import string
 import os
 
-from dataBasic import dibuixaMapa, creaGraf, connectedComponents, nodesGraph, edgesGraph
+from data import dibuixaMapa, creaGraf, connectedComponents, nodesGraph, edgesGraph, shortestPath
 
 '''
 Coses que falten a fer:
-    --funcio route
-    --comprovar que funciona per més d'un usuari a la vegada
     --acabar d'arreglar els textos
     --fer alguna cosa extra per anar de sobrats(amb la capacitat de cada estacio o la altitud)
     --graf lineal
+    --treure els chivatos
+    --editar bot
 
-Preguntar:
-    --Esta be el graf per defecte
-    --Es pot fer servir alguna variable global
-    --Esta be usar un diccionari per controlar els usuaris
+    #POTSER CAL UNA TOLERANCIA PER A LES COMPARACIONS (???)
+    #CAL MIRAR QUAN HEM DE FER READ_DATAs
+    #FALTA DOCUMENTAR
 
 '''
-
-#textos
-startText = "Hola! Sóc un bot del Bicing de Barcelona. \nPer més informació escriu /help."
-helpText = "Coses que puc fer i presentacio i ajuda i tal i cual"
-authorsText = "Els meus autors són: \nMarc Gallego: marc.gallego.asin@est.fib.upc.edu \nMarc Vernet: marc.vernet@est.fib.upc.edu"
-
-usuaris = dict()
 
 def randomName():
     stringLength = 10
@@ -35,59 +27,93 @@ def randomName():
     r = ''.join(random.choice(letters) for i in range(stringLength))
     return (r + '.png')
 
-def start(bot, update):
+
+def start(bot, update, user_data):
+    startText = "Hola " + update.message.chat.first_name + "!😄 Sóc un bot del Bicing de Barcelona.\
+    \nEt puc ajudar a buscar rutes i moltes coses més!😏😏 \nPer més informació escriu /help."
     bot.send_message(chat_id=update.message.chat_id, text=startText)
-    usuaris[update.message.chat_id] = creaGraf(1000)
+    user_data['graf'] = creaGraf(1000)
+
 
 def help(bot, update):
+    helpText = "Coses que puc fer: \n👨‍💻 /authors: nn. \n🖍️ /graph <distància>: nn. \n⭕ /nodes: nn. \
+    \n↗️ /edges: nn. \n🔄 /components: nn. \n🗺️ /plotgraph: nn. \n🚴‍♀️ /route: nn."
     bot.send_message(chat_id=update.message.chat_id, text=helpText)
 
+
 def authors(bot, update):
+    authorsText = "Els meus autors són: \nMarc Gallego🤓: marc.gallego.asin@est.fib.upc.edu \
+    \nMarc Vernet😎: marc.vernet@est.fib.upc.edu"
     bot.send_message(chat_id=update.message.chat_id, text=authorsText)
 
-def graph(bot, update, args):
-    usuaris[update.message.chat_id] = creaGraf(int(args[0]))
-    bot.send_message(chat_id=update.message.chat_id, text="graf fet")
 
-def nodes(bot, update):
-    n = nodesGraph(usuaris[update.message.chat_id])
+def graph(bot, update, args, user_data):
+    bot.send_message(chat_id=update.message.chat_id, text="⏲️Creant graf...")
+    try:
+        user_data['graf'] = creaGraf(int(args[0]))
+        bot.send_message(chat_id=update.message.chat_id, text="✔️Graf creat!")
+    except:
+        bot.send_message(chat_id=update.message.chat_id, text="💀Alguna cosa ha fallat...")
+
+
+def nodes(bot, update, user_data):
+    n = nodesGraph(user_data['graf'])
     bot.send_message(chat_id=update.message.chat_id, text=n)
 
-def edges(bot, update):
-    n = edgesGraph(usuaris[update.message.chat_id])
+
+def edges(bot, update, user_data):
+    n = edgesGraph(user_data['graf'])
     bot.send_message(chat_id=update.message.chat_id, text=n)
 
-def components(bot, update):
-    n = connectedComponents(usuaris[update.message.chat_id])
+
+def components(bot, update, user_data):
+    n = connectedComponents(user_data['graf'])
     bot.send_message(chat_id=update.message.chat_id, text=n)
 
-def plotgraph(bot, update):
-    photoName = randomName()
-    dibuixaMapa(usuaris[update.message.chat_id], photoName)
-    bot.send_photo(chat_id=update.message.chat_id, photo=open(photoName, 'rb'))
-    os.remove(photoName)
 
-def route(bot, update, args):
-    shortestPath(usuaris[update.message.chat_id], args)
-    bot.send_message(chat_id=update.message.chat_id, text="algo")
+def plotgraph(bot, update, user_data):
+    bot.send_message(chat_id=update.message.chat_id, text="🏗️Construint mapa...")
+    try:
+        photoName = randomName()
+        dibuixaMapa(user_data['graf'], photoName)
+        bot.send_photo(chat_id=update.message.chat_id, photo=open(photoName, 'rb'))
+        os.remove(photoName)
+    except:
+        bot.send_message(chat_id=update.message.chat_id, text="💀Alguna cosa ha fallat...")
+
+
+def route(bot, update, args, user_data):
+    bot.send_message(chat_id=update.message.chat_id, text="⏲️Calculant ruta...")
+    try:
+        photoName = randomName()
+        str = (" ".join(args))
+        shortestPath(user_data['graf'], str, photoName)
+        bot.send_photo(chat_id=update.message.chat_id, photo=open(photoName, 'rb'))
+        os.remove(photoName)
+    except:
+        bot.send_message(chat_id=update.message.chat_id, text="💀Alguna cosa ha fallat...")
+
 
 # declara una constant amb el access token que llegeix de token.txt
 TOKEN = open('token.txt').read().strip()
+
 
 # crea objectes per treballar amb Telegram
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
+
 # indica que quan el bot rebi la comanda  s'executi la funció
-dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('start', start, pass_user_data=True))
 dispatcher.add_handler(CommandHandler('help', help))
 dispatcher.add_handler(CommandHandler('authors', authors))
-dispatcher.add_handler(CommandHandler('graph', graph, pass_args=True))
-dispatcher.add_handler(CommandHandler('nodes', nodes))
-dispatcher.add_handler(CommandHandler('edges', edges))
-dispatcher.add_handler(CommandHandler('components', components))
-dispatcher.add_handler(CommandHandler('plotgraph', plotgraph))
-dispatcher.add_handler(CommandHandler('route', route, pass_args=True))
+dispatcher.add_handler(CommandHandler('graph', graph, pass_args=True, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('nodes', nodes, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('edges', edges, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('components', components, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('plotgraph', plotgraph, pass_user_data=True))
+dispatcher.add_handler(CommandHandler('route', route, pass_args=True, pass_user_data=True))
+
 
 # engega el bot
 updater.start_polling()
--- data.py
+++ data.py
@@ -1,14 +1,46 @@
 import pandas as pd
 import networkx as nx
-import haversine as hs
-import geopy as gp
-import staticmap as smap
+import os
+
+from pandas import DataFrame, Series
+from haversine import haversine
+
+from geopy.geocoders import Nominatim
+from staticmap import *
+import string
+
+
+def readData():
+    url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
+    bicing = pd.DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
+    bicing = bicing.drop(['physical_configuration', 'capacity', 'altitude', 'post_code', 'name', 'cross_street'], axis=1)
+    return bicing
+bicing = readData()
+
+
+def swap(coords): # returns (lat, lon)
+    return coords[::-1]
+
+
+def getCoords(id, bicing):
+    lon = bicing.loc[id, "lon"]
+    lat = bicing.loc[id, "lat"]
+    return (lon, lat)
+
+
+def distance(origen, desti):
+    coordA = swap(getCoords(origen, bicing))
+    coordB = swap(getCoords(desti, bicing))
+    return haversine(coordA, coordB, unit='m')
+
+
+def walkTime(coordsA, coordsB):
+    speed = 4*1000/3600
+    distance = haversine(coordsA, coordsB, unit='m')
+    return distance / speed
+
 
 def addressesTOcoordinates(addresses):
-    '''
-    Returns the two coordinates of two addresses of Barcelona
-    in a single string separated by a comma. In case of failure, returns None.
-    '''
     try:
         geolocator = Nominatim(user_agent="bicing_bot")
         address1, address2 = addresses.split(',')
@@ -19,10 +51,123 @@ def addressesTOcoordinates(addresses):
         return None
 
 
-def main():
-    url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
-    bicing = pd.DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
+def drawPath(path, coordsST, photoName):
+    mida = 1500
+    diameter = mida // 180
+    thickness = mida // 300
+    m = StaticMap(mida, mida, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+    n = len(path)
+
+    #Plotting vertices:
+    m.add_marker(CircleMarker(swap(coordsST[0]), 'black', diameter*2))
+    m.add_marker(CircleMarker(swap(coordsST[0]), 'white', diameter))
+
+    m.add_marker(CircleMarker(swap(coordsST[1]), 'black', diameter*2))
+    m.add_marker(CircleMarker(swap(coordsST[1]), 'white', diameter))
+    for id in path[1:-1]:
+        coords = getCoords(id, bicing)
+        marker = CircleMarker(coords, 'black', diameter)
+        m.add_marker(marker)
+
+    if n == 2:
+        m.add_line(Line((swap(coordsST[0]), swap(coordsST[1])), 'red', thickness))
+    else:
+        m.add_line(Line((swap(coordsST[0]), getCoords(path[1],bicing)), 'red', thickness))
+        for i in range(2, n-1):
+            coordsA = getCoords(path[i-1], bicing)
+            coordsB = getCoords(path[i], bicing)
+            m.add_line(Line(((coordsA), (coordsB)), 'blue', thickness))
+        m.add_line(Line((swap(coordsST[1]), getCoords(path[-2],bicing)), 'red', thickness))
+    image = m.render()
+    image.save(photoName)
+    print('Done!')
+
+
+def shortestPath(G, addresses, photoName):
+    print("entra shortest path")
+
+    coordsST = addressesTOcoordinates(addresses)
+    if(coordsST == None):
+        return None
+    G.add_nodes_from(('source', 'target'))
+
+    #bicing = readData()
+    station_ids = bicing.index.tolist()
+    for id in station_ids:
+        idCoords = swap(getCoords(id, bicing))
+        G.add_edge('source', id, weight = walkTime(coordsST[0], idCoords))
+        G.add_edge('target', id, weight = walkTime(coordsST[1], idCoords))
+
+    weightST = walkTime(coordsST[0], coordsST[1])
+    G.add_edge('source', 'target', weight=weightST)
+
+    p = nx.dijkstra_path(G,'source','target','weight')
+    drawPath(p, coordsST, photoName)
+
+    G.remove_nodes_from(('source', 'target'))
+    return p
+
+
+def creaGraf(max_dist):
+    print('entra')
     G = nx.Graph()
+    visitat = dict()
+    station_ids = bicing.index.tolist()
+    speed = 10*1000/3600
+
+    for id in station_ids:
+        G.add_node(id)
+        visitat.update({id: False})
+    for origen in station_ids:
+        visitat[origen] = True;
+        for desti in station_ids:
+            if (not visitat[desti]):
+                dist = distance(origen, desti)
+                if (dist <= max_dist):
+                    G.add_edge(origen, desti, weight = dist/speed)
+    print('graf fet')
+    return G
 
 
-main()
+def dibuixaMapa(G, photoName):
+    print('entra mapa')
+    midaX = midaY = 1500
+    diametre = midaX // 180
+    m = StaticMap(midaX, midaY, url_template='http://a.tile.osm.org/{z}/{x}/{y}.png')
+
+    nodes = list(G.nodes)
+    for node in nodes:
+        coords = getCoords(node, bicing)
+        m.add_marker(CircleMarker(coords, 'black', diametre*2))
+        m.add_marker(CircleMarker(coords, 'white', diametre))
+    print('nodes fets')
+
+    edges = list(G.edges.data())
+    gruix = midaX // 300
+    for edge in edges:
+        origen = edge[0]
+        desti  = edge[1]
+
+        coorA = getCoords(origen, bicing)
+        coorB = getCoords(desti, bicing)
+        m.add_line(Line(((coorA), (coorB)), 'blue', gruix))
+
+    print('arestes fets')
+
+    image = m.render()
+    print('render fets')
+
+    image.save(photoName)
+
+    print('mapa fet')
+
+
+#Potser estaria be posar aqui dues funcions tontes que diguin edges i nodes (per cohesio)
+def connectedComponents(G):
+    return nx.number_connected_components(G)
+
+def nodesGraph(G):
+    return G.number_of_nodes()
+
+def edgesGraph(G):
+    return G.number_of_edges()
